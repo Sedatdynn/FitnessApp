37290ce3daa99d6b0e2e8012c8d9a8270c569851
Sedat Dayan <dayan.sedat1998@gmail.com>
2024-05-20 14:49:29 +0300

    sliver usage instead of listViewBuilder

--- fitness_app/lib/feature/home/exercises/shimmer/exercise_shimmer.dart
+++ fitness_app/lib/feature/home/exercises/shimmer/exercise_shimmer.dart
@@ -1,5 +1,5 @@
+import 'package:fistness_app_firebase/feature/auth/age/age_shelf.dart';
 import 'package:fistness_app_firebase/product/theme/colors.dart';
-import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
 class ExerciseViewShimmer extends StatelessWidget {
@@ -10,35 +10,41 @@ class ExerciseViewShimmer extends StatelessWidget {
     return Shimmer.fromColors(
       baseColor: Colors.grey.shade300,
       highlightColor: AppColors.mainPrimary,
-      child: ListView.builder(
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: Container(
-              decoration: BoxDecoration(
-                color: AppColors.white,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              width: 60,
-              height: 50,
+      child: SizedBox(
+        height: context.dynamicHeight(1),
+        child: CustomScrollView(
+          slivers: [
+            SliverList.list(
+              children: List.generate(20, (_) {
+                return ListTile(
+                  leading: Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    width: 60,
+                    height: 50,
+                  ),
+                  title: Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    width: 60,
+                    height: 50,
+                  ),
+                  trailing: const SizedBox(
+                    height: 10,
+                    width: 10,
+                    child: Icon(
+                      Icons.chevron_right_outlined,
+                    ),
+                  ),
+                );
+              }),
             ),
-            title: Container(
-              decoration:
-                  BoxDecoration(color: AppColors.white, borderRadius: BorderRadius.circular(8)),
-              width: 60,
-              height: 50,
-            ),
-            trailing: const SizedBox(
-              height: 10,
-              width: 10,
-              child: Icon(
-                Icons.chevron_right_outlined,
-              ),
-            ),
-          );
-        },
+          ],
+        ),
       ),
     );
   }
